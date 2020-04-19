dc6903f5835ed9db476881703011d40ae2f9259b
XingMengL <1156351528@qq.com>
2020-04-19 23:48:59 +0800

    一维数组赋值

---
+++ 习题/乙未数组”赋值“.cpp
@@ -0,0 +1,17 @@
+#include "stdio.h"
+#define n 100
+int main()
+{
+	int i, j;
+	int a[n];
+	scanf("%d", &j);
+	for( i = 0; i < j; i++ ){
+		scanf("%d", &a[i]);
+		if( )
+		); 
+	}
+	
+	
+	return 0;
+	
+}
